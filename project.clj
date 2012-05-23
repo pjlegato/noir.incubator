69ea9d635854035e52fa1567afd8ab6f008690ec
Paul Legato <pjlegato@builtonlogic.com>
2012-05-22 20:45:16 -0700

    Log4j logging middleware

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject noir.incubator "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :plugins [[lein-swank "1.4.4"]]
+  :plugins [[lein-swank "1.4.4"]
+            [lein-git-deps "0.0.1-SNAPSHOT"]]
+
   :dependencies [[org.clojure/clojure "1.3.0"]
 
                  ;; Logging infrastructure
                  [org.slf4j/slf4j-log4j12 "1.6.4"]
                  [org.clojure/tools.logging "0.2.3"]
                  [clj-logging-config "1.9.6"]
+                 [org.clojars.pjlegato/clansi "1.3.0"]
                  
                  ])
--- src/noir/incubator/logging.clj
+++ src/noir/incubator/logging.clj
@@ -3,8 +3,6 @@
   (require
    [clj-logging-config.log4j :as log-config]))
 
-;; TODO: Add colorization.
-
 ;; TODO: rename according to development/testing/production mode
 (def base-log-name "logs/noir.log")
 
@@ -14,6 +12,7 @@
 ;; performance is an issue. See
 ;; http://logging.apache.org/log4j/companions/extras/apidocs/org/apache/log4j/EnhancedPatternLayout.html
 ;; for information on which fields are slow.
+;;
 (def debugging-log-prefix-format "%d %l [%p] : %throwable%m%n")
 (def production-log-prefix-format "%d [%p] : %throwable%m%n")
 
@@ -26,7 +25,8 @@
 ;; - http://logging.apache.org/log4j/1.2/apidocs/org/apache/log4j/AppenderSkeleton.html for different log destinations
 ;; - http://logging.apache.org/log4j/companions/extras/apidocs/org/apache/log4j/EnhancedPatternLayout.html for prefix formatting options
 ;;
-
+;; TODO: Make a custom layout class that colorizes the log level. Maybe this can be done in a filter.
+;;
 (def rotating-logger
   "This logging adapter rotates the logfile nightly at about midnight."
   (org.apache.log4j.DailyRollingFileAppender.
@@ -38,10 +38,16 @@
   "This logging adapter simply appends new log lines to the existing logfile."
   (org.apache.log4j.FileAppender.
    (org.apache.log4j.EnhancedPatternLayout. production-log-prefix-format)
-   "logs/noir.log"
+   base-log-name
    true))
 
 ;; TODO: Auto-detect production vs. development mode, and reduce the log level in production mode.
 ;; TODO: Provide hooks for the user's Noir application to select the desired logging adapter.
-(log-config/set-logger! :level :debug
-                        :out appending-logger)
+(defn setup-default-logger!
+  "Initializes the logger for the current namespace. This must be
+  called once per namespace that plans to log things."
+  []
+  (log-config/set-logger! (str *ns*)
+                          :level :debug
+                          :out appending-logger))
+
